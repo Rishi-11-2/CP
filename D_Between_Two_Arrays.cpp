4a360cac39a02368c0ec252876b840dd2cb6e58e
Rishi <rishichowdhury711@gmail.com>
2023-11-06 22:19:13 +0530

    Problems

--- D_Between_Two_Arrays.cpp
+++ D_Between_Two_Arrays.cpp
@@ -25,6 +25,8 @@ void solve()
     vector<vector<long long>>prev(n+1,vector<long long>(3000+1,0));
     dp[n][0]=1LL;
     prev[n][0]=1LL;
+    /* Converting from recursive to iterative
+    took almost 5 days . Had to think about how to optimise the code*/
     for(long long i=1;i<=3000;i++)
     {
         dp[n][i]=1LL;
@@ -33,7 +35,11 @@ void solve()
     for(long long i=n-1;i>=0;i--)
     {
         dp[i][0]=prev[i+1][b[i]]%mod;
-        for(long long j=a[i];j<=b[i];j++)
+        if(a[i]>0)
+        {
+            dp[i][0]=(dp[i][0]-prev[i+1][a[i]-1]+mod)%mod;
+        }
+        for(long long j=1;j<=3000;j++)
         {
             dp[i][j]=prev[i+1][b[i]]%mod;
             long long x=j-1;
@@ -56,11 +62,6 @@ void solve()
     //     cout<<dp[i][j]<<" ";
     //     cout<<endl;
     // }
-    long long res=0;
-    for(int i=a[0];i<=b[0];i++)
-    {
-        res=(res%mod+dp[0][i]%mod)%mod;
-    }
-    cout<<res;
+    cout<<dp[0][0]%mod<<endl;
     cout<<endl;
 }
